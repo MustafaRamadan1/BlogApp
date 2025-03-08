1eb8940334f7bd4c00ebba9ea6d80e64908e7554
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2025-03-08 02:42:10 +0200

    feat: add author reference to post schema

--- src/post/schemas/post.schema.ts
+++ src/post/schemas/post.schema.ts
@@ -1,4 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { User } from '../../auth/schemas/user.schema';
+import { Schema as mongooseSchema } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class Post {
@@ -13,6 +15,12 @@ export class Post {
     minlength: [3, 'Content must be at least 3 characters'],
   })
   content: string;
+  @Prop({
+    type: mongooseSchema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'post must belongs to an author'],
+  })
+  author: User;
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
