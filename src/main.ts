6a7075846b29517ab9c11da81b68e59812a7335a
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2025-03-08 18:53:22 +0200

    feat: enable CORS with specific origins and methods in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:5173'], // Allow specific origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+    credentials: true, // Allow sending cookies
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
