5421a5f61cf4d27cc709fcadef2ed363337e3774
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2025-03-08 06:53:55 +0200

    feat: implement CommentController with methods for retrieving and creating comments

---
+++ src/comment/comment.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { Query as ExpressQuery } from 'express-serve-static-core';
+import { Comment } from './schemas/comment.schema';
+import { AuthGuard } from '@nestjs/passport';
+import { createCommentDto } from './dto/create-comment.dto';
+
+@Controller('api/v1/comments')
+export class CommentController {
+  constructor(private commentService: CommentService) {}
+
+  @Get(':postId')
+  async getPostComments(
+    @Param('postId') postId: string,
+    @Query() query: ExpressQuery,
+  ): Promise<{ result: number; comments: Comment[] }> {
+    const comments = await this.commentService.findPostComments(postId, query);
+    return { result: comments.length, comments };
+  }
+
+  @Post()
+  @UseGuards(AuthGuard())
+  async create(
+    @Body() comment: createCommentDto,
+    @Req() req,
+  ): Promise<Comment> {
+    const newComment = await this.commentService.create(comment, req.user);
+    return newComment;
+  }
+
+  //   @Get(':id')
+  //   async GetPost(@Param('id') id: string): Promise<PostDocument | null> {
+  //     const post = await this.PostService.findById(id);
+  //     if (!post) {
+  //       throw new NotFoundException('Post  Not Found');
+  //     }
+
+  //     return post;
+  //   }
+}
